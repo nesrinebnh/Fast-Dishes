6fc564ef103e4f7995a6d813bce5a73d9bd49447
Nesrine Bounouh <43906541+nesrinebnh@users.noreply.github.com>
2022-04-01 12:55:29 +0200

    extract recipe details

--- crawler/recipescraper/recipescraper/items.py
+++ crawler/recipescraper/recipescraper/items.py
@@ -10,12 +10,28 @@ from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
 
 
+def parse_Level(level):
+    level = level.replace(' mins ', '')
+    l = level.split('hrs')
+    l = [int(element.strip()) for element in l]
+    duration  = level
+    if len(l) > 0:
+        duration = l[0]*60 + l[1]
+    return str(duration)
+
+def parseSpecial(text):
+    text = text.replace('\u2018', "'")
+    text = text.replace("\u2019", "'")
+    text = text.replace("&amp;", "&")
+    text = text.replace("\u2013", "-")
+    return text
+
 class RecipescraperItem(scrapy.Item):
     # define the fields for your item here like:
     
-    title = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor=TakeFirst())
-    description = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor=TakeFirst())
+    title = scrapy.Field(input_processor = MapCompose(remove_tags,parseSpecial), output_processor=TakeFirst())
+    description = scrapy.Field(input_processor = MapCompose(remove_tags,parseSpecial), output_processor=TakeFirst())
     url = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor=TakeFirst())
-    
-    
-    pass
+    image_url = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor=TakeFirst())
+    level= scrapy.Field(input_processor = MapCompose(remove_tags), output_processor=TakeFirst())
+    duration = scrapy.Field(input_processor = MapCompose(remove_tags, parse_Level), output_processor=TakeFirst())
--- crawler/recipescraper/recipescraper/spiders/recipespider.py
+++ crawler/recipescraper/recipescraper/spiders/recipespider.py
@@ -22,10 +22,29 @@ class RecipeSpider(scrapy.Spider):
             I.add_xpath('title', 'header/h4/a')
             I.add_xpath('description', 'p')
             I.add_xpath('url', 'header/h4/a/@href')
-            yield I.load_item()
+            url = recipes.xpath('header/h4/a/@href').getall()[0]
+            print('=============================')
+            print(url)
+            print('==============================')
+            #yield response.follow(author_url, self.parse_author, meta={'quote_item': quote_item})
+            # yield I.load_item()
+            recipeItem = I.load_item()
+            yield response.follow(url, self.parseDetail, meta={'recipe_item': recipeItem})
             
         next_page = response.xpath('//*/main/div[@class="pagination"]/ol/li/a[@class="next"]/@href').get()
         
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
-            
+    
+    def parseDetail(self, response):
+        recipe_item = response.meta['recipe_item']
+        loader = ItemLoader(item=recipe_item, response=response)
+        # print('============================')
+        # print('image url', response.xpath('//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-4"]/div[@class="img-holder"]/img/@src').get())
+        # print('level', response.xpath('//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-8"]/div[@class="preparation"]/ul/li[2]/span[@class="text"]/text()').get())
+        # print('duration', response.xpath('//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-8"]/div[@class="preparation"]/ul/li[1]/span/text()[3]').get())
+        # print('===========================')
+        loader.add_xpath('image_url', '//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-4"]/div[@class="img-holder"]/img/@src')
+        loader.add_xpath('level', '//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-8"]/div[@class="preparation"]/ul/li[2]/span[@class="text"]')
+        loader.add_xpath('duration','//*[@id="main"]/div[@class="recipe-head-container"]/div[@class="row remove_margin"]/div[@class="col-md-8"]/div[@class="preparation"]/ul/li[1]/span/text()[3]')
+        yield loader.load_item()
