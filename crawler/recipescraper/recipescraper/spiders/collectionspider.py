341960692bdd92be2ad06dc3c498bd2d6c362a1b
Nesrine Bounouh <43906541+nesrinebnh@users.noreply.github.com>
2022-04-14 09:21:22 +0200

    get collections urls from db

--- crawler/recipescraper/recipescraper/spiders/collectionspider.py
+++ crawler/recipescraper/recipescraper/spiders/collectionspider.py
@@ -2,12 +2,31 @@ from gc import callbacks
 import scrapy
 from recipescraper.items import CollectionItem
 from scrapy.loader import ItemLoader
+import mysql.connector
+
+def getCollectionUrls():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="scrapy_recipes"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM collections")
+
+    myresult = mycursor.fetchall()
+
+    l = []
+    for x in myresult:
+        l.append(x[1])
+    
+    return l
 
 class CollectionSpider(scrapy.Spider):
     name = 'collection'
-    start_urls = [
-                  'https://www.bbcgoodfoodme.com/collections/',
-                  ]
+    start_urls = getCollectionUrls()
     
     def parse(self, response):
         for collection in response.xpath('//*[@id="main"]/div[1]/ul/li'):
